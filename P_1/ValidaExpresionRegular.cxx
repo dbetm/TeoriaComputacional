72bf4f7a5149054d3825daa07795fea75368a1e4
David <davbetm@gmail.com>
2017-09-09 12:20:10 -0500

    Experimental stage, test correct: Found all meta-characters of regularExpression

--- P_1/ValidaExpresionRegular.cxx
+++ P_1/ValidaExpresionRegular.cxx
@@ -7,19 +7,22 @@ class Validator {
     private:
         string regularExpression;
         string str;
+        int posChars;
     public:
         Validator();
         void setRegularExpression(string);
-        void setCadena(string);
+        void setStr(string);
         bool valida();
-        int findMeta(pos); //Va buscando meta-carácteres y retorna sus posciones
-        bool filertAst();
+        int findMeta(int); //Va buscando meta-carácteres y retorna sus posciones
+        bool filterAst(int, char);
+        bool filterPlus(int, char);
         ~Validator();
 };
 
 Validator::Validator() {
     this->regularExpression = "";
     this->str = "";
+    posChars = 0;
 }
 
 void Validator::setRegularExpression(string expression) {
@@ -27,74 +30,98 @@ void Validator::setRegularExpression(string expression) {
     cout << this->regularExpression << endl;
 }
 
-void Validator::setCadena(string str) {
+void Validator::setStr(string str) {
     this->str = str;
 }
 
 bool Validator::valida() {
-    int pos = 0
-    bool answer = false;
+    int pos = 0;
+    bool answer = true;
 
-    //while(false) {}
-    pos = findMeta(pos);
-
-    switch (regularExpression[pos]) {
-        case '*':
-            cout << "Encontré un *" << endl;
-            break;
-        case '+':
-            if(!filertAst(pos)) {
-                answer = false;
+    while(true) {
+        pos = findMeta(pos);
+        cout << regularExpression[pos] << endl;
+        //cout << "La posición en la expresión es: " << pos << endl;
+        switch(regularExpression[pos]) {
+            case '*':
+                if(!filterAst(pos, regularExpression[pos - 1])) {
+                    answer = false;
+                }
                 break;
-            }
-        case '|':
+            case '+':
+                if(!filterPlus(pos, regularExpression[pos - 1])) {
+                    answer = false;
+                }
+                break;
+            case '|':
+                break;
+            case '?':
+                break;
+            default:
+                break;
+        }
+        if(answer == false || pos == regularExpression.length() - 1) {
             break;
-        case '?':
-            break;
-        default:
-            break;
+        }
     }
-    
     return answer;
 }
-
+//Se utiliza esta función para encontrar las posiciones de los meta-carácteres  en la expresión regular
 int Validator::findMeta(int pos) {
-    for (int i = pos; i < str.size(); i++) {
-        if(str[i] == '*') {
+
+    for(int i = pos + 1; i < regularExpression.length(); i++) {
+        if(regularExpression[i] == '*') {
             pos = i;
             break;
         }
-        else if(str[i] == '|') {
+        else if(regularExpression[i] == '|') {
             pos = i;
             break;
         }
-        else if(str[i] == '+') {
+        else if(regularExpression[i] == '+') {
             pos = i;
             break;
         }
-        else if(str[i] == '?') {
+        else if(regularExpression[i] == '?') {
             pos = i;
             break;
         }
-        else if(str[i] == '.') {
+        else if(regularExpression[i] == '.') {
             pos = i;
             break;
         }
     }
-    //int posFirstMetaChar = regularExpression.find_first_of("*|+.?");
     return pos;
 }
 
-bool Validator::filertAst() {
-
+bool Validator::filterAst(int pos, char simbol) {
+    return true;
 }
 
-Validator::~Validator(){}
+bool Validator::filterPlus(int pos, char simbol) {
+    bool answer = true;
+    int cont = 0;
+    int i = posChars;
+    while (str[i] == simbol) {
+        cont++;
+        i++;
+    }
+    posChars = i;
+    if(cont < 1) answer = false;
+    //cout << "Contador vale: " << cont << endl;
+    return answer;
+}
+
+Validator::~Validator() {}
 
 int main() {
     Validator v;
     bool x;
-    v.setRegularExpression("a+.b*");
+    v.setRegularExpression("a+.b*.c.b|d.x?");
+    v.setStr("aacb");
     x = v.valida();
+    if(!x) cout << "No se acepta" << endl;
+    else cout << "Se acepta" << endl;
+
     return 0;
 }
